63ba5563a99f72579178becdb615a0f504bc0b7c
Savo Shen <savo_shen@qq.com>
2025-04-12 23:12:22 +0800

    删除测试图片

---
+++ Animal_Recognition/Welcome.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome.swift
+//  Animal_Recognition
+//
+//  Created by 沈逸帆 on 2025/4/12.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome()
+}
